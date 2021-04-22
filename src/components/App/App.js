3f1383e08b9a56991220ec6ed7bca6d08a95c803
Edik-Litvinov <e.litvinov00@yandex.by>
2021-04-22 17:23:30 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className='page'>
+      <div className='page__container'>
+        fsdfsdf
+      </div>
+    </div>
+  );
+}
+
+export default App;
